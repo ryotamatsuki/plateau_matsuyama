37a9a08df9468f3f57d6d6db2cfa7538d83b8ded
ryotamatsuki <60263857+ryotamatsuki@users.noreply.github.com>
2026-09-16 12:54:42 +0900

    Add flight camera and responsive ground navigation
    * Implement flight camera navigation controller
    
    * Load camera navigation controller
    
    * Add camera navigation browser tests
    
    * Validate camera navigation in CI
    
    * Run camera navigation checks on Pages deployment

--- docs/thematic-bootstrap.js
+++ docs/thematic-bootstrap.js
@@ -1,5 +1,13 @@
 'use strict';
 
+(function loadNavigationController(){
+  if (document.querySelector('script[data-matsuyama-navigation]')) return;
+  const script = document.createElement('script');
+  script.src = 'navigation-controller.js';
+  script.dataset.matsuyamaNavigation = '1';
+  document.head.appendChild(script);
+})();
+
 (function waitForViewer() {
   const viewer = window.__matsuyamaViewer;
   if (!viewer || viewer.isDestroyed?.()) {
